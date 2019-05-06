8f5978b78d282489b6413cce09ae417509e7862b
Your Name <you@example.com>
2019-05-06 14:52:12 +0800

    完成首页左侧样式

--- src/components/index.vue
+++ src/components/index.vue
@@ -14,7 +14,21 @@
       </el-row>
     </el-header>
     <el-container class="container">
-      <el-aside width="200px" class="aside">Aside</el-aside>
+      <!-- 左侧 -->
+      <el-aside width="200px" class="aside">
+        <el-menu default-active="2" class="el-menu-vertical-demo">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>用户列表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 左侧 -->
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -29,16 +43,16 @@ export default {
 <style lang='scss'>
 .father-box {
   height: 100%;
-  .header{
-      background: #bbccdd
+  .header {
+    background: #bbccdd;
   }
-  .container{
-      .aside{
-          background: pink;
-      }
-      .main{
-          background: green;
-      }
+  .container {
+    .aside {
+      background: skyblue;
+    }
+    .main {
+      background: green;
+    }
   }
 }
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="login">
     <div class="center-box">
-        <h2>用户登录</h2>
+      <h2>用户登录</h2>
       <el-form label-position="top" label-width="80px">
         <el-form-item label="用户名">
-          <el-input></el-input> 
+          <el-input></el-input>
         </el-form-item>
         <el-form-item label="密码">
           <el-input></el-input>
@@ -22,34 +22,34 @@ export default {};
 </script>
 
 <style lang='scss'>
-html,body{
-    padding:0;
-    margin: 0;
+html,
+body {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  .login {
     height: 100%;
-    .login{
-        height: 100%;
-        background: #324152;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+    background: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 
-.center-box{
-    width: 480px;
-    height:360px;
-    background: white;
-    border-radius: 10px;
-    padding: 20px 35px;
-    box-sizing: border-box;
-    .my-btn{
-        width: 100%;
-        margin-top: 10px;
-    }
-    h2{
-        margin: 0;
-        text-align: center;
-    }
+.center-box {
+  width: 480px;
+  height: 360px;
+  background: white;
+  border-radius: 10px;
+  padding: 20px 35px;
+  box-sizing: border-box;
+  .my-btn {
+    width: 100%;
+    margin-top: 10px;
+  }
+  h2 {
+    margin: 0;
+    text-align: center;
+  }
 }
-
 </style>
