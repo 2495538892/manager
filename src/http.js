8de3587d8e558aa8e75f5642cc2360ffea3d869e
Your Name <you@example.com>
2019-05-08 16:54:00 +0800

    设置请求拦截器,统一设置token

--- src/http.js
+++ src/http.js
@@ -4,17 +4,23 @@ import axios from 'axios';
 // 设置基地址
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 
+// 请求拦截器===回调函数,请求的时候携带Authorization=token ,这样就可以统一设置token
+axios.interceptors.request.use(function (config) {
+    console.log(config)
+    config.headers.Authorization = sessionStorage.getItem('token')
+    return config;
+}, function (error) {
+    return error
+});
+
+
 const request = {
     login(params) {
         return axios.post('login', params);
     },
     // 封装获取用户列表的请求
     getuser(params) {
-        return axios.get('users', {
-            params, headers: { //第一个为需要传入的参数,第二个文档规定要设置;
-                Authorization: sessionStorage.getItem('token')
-            }
-        })
+        return axios.get('users', { params })
     }
 }
 
